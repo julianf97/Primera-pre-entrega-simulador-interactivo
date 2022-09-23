2f0a4dea4ee4e29ae723ac85e65012d92f6f87a0
Julian Finelli <julianfinelli97@gmail.com>
2022-09-23 11:12:05 -0300

    commit unico

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Puse el algoritmo dentro de una función, no se si es correcto eso o no.
+const reproductorDeVideos = () => {
+    
+    // El algoritmo corresponde al reproductor de videos que tiene el index, la idea sería que cuando el usuario clickee en las imagenes miniaturas debajo del video principal, el video principal cambie por el video que corresponde a la imagen en miniatura que el usuario eligió.
+    
+    switch(videos){
+        case 0:
+        // este case correspondería a la primera imagen en miniatura que a su vez corresponde al video que está como principal ya definido, osea que cuando clickee en este caso no quiero que haga nada porque ya es el video que estaría reproduciendose.
+        break;
+        case 1:
+        // este case correspondería a la segunda imagen en miniatura, cuando el usuario clickee en la segunda imagen en miniatura quiero que este case cambie el video principal que está reproduciendose por el video que corresponde a la segunda imagen en miniatura.
+        break;
+        case 2:
+        // este case correspondería a la tercer imagen en miniatura, cuando el usuario clickee en la tercer imagen en miniatura quiero que este case cambie el video principal que está reproduciendose por el video que corresponde a la tercer imagen en miniatura.
+        break;
+        case 3:
+        // este case correspondería a la cuarta imagen en miniatura, cuando el usuario clickee en la cuarta imagen en miniatura quiero que este case cambie el video principal que está reproduciendose por el video que corresponde a la cuarta imagen en miniatura.
+        break;
+        default:
+        break;
+    }
+}
+
+
